0c33e68fc90158fc95e67f7682fba26c5df50fdc
Azucena López <blankazucenalg@gmail.com>
2015-11-12 01:12:49 -0600

    Plot multiple decision boundaries

--- Pr3-Adaline/Plotter.py
+++ Pr3-Adaline/Plotter.py
@@ -22,34 +22,19 @@ class Plotter:
             ax = fig.gca(projection='3d')  # to work in 3d
             plt.hold(True)
 
-            a = round(weights[0][0], 4)
-            b = round(weights[0][1], 4)
-            c = round(weights[0][2], 4)
-            d = round(threshold[0][0], 4)
-
-            if c != 0:
-                x = np.arange(-3, 3, 1)  # generate a mesh
-                y = np.arange(-3, 3, 1)
-                x, y = np.meshgrid(x, y)
-                z = (-a * x - b * y - d) / c
-            elif b != 0:
-                x = np.arange(-3, 3, 1)  # generate a mesh
-                z = np.arange(-3, 3, 1)
-                x, z = np.meshgrid(x, z)
-                y = (-a * x - c * z - d) / b
-            else:
-                y = np.arange(-3, 3, 1)  # generate a mesh
-                z = np.arange(-3, 3, 1)
-                y, z = np.meshgrid(y, z)
-                x = (-b * y - c * z - d) / a
-
-            ax.plot_surface(x, y, z, color='c', alpha=0.3)  # plot a 3d surface plot
+            # Plot each decision boundary
+            for i in range(rows):
+                a = round(weights[i][0], 4)
+                b = round(weights[i][1], 4)
+                c = round(weights[i][2], 4)
+                d = round(threshold[i][0], 4)
+                self.plot_boundary(ax, a, b, c, d)
 
             # Plot the inputs
             c = ['r', 'b', 'g', 'c', 'm', 'y', 'k', 'w']
             m = ['o', '^', 's', 'v', '8', 'p', '*', 'h', 'H', 'D', 'd', '+', 'x', '<', '>']
             for i, t in zip(inputs, targets):
-                ax.scatter(i[0], i[1], i[2], c=c[int(round(t)) % len(c)], marker=m[int(round(t)) % len(m)])
+                ax.scatter(i[0], i[1], i[2], c=c[int(round(sum(t))) % len(c)], marker=m[int(round(sum(t))) % len(m)])
 
             ax.set_xlabel('x label')
             ax.set_ylabel('y label')
@@ -57,3 +42,22 @@ class Plotter:
             ax.set_title(title)
             plt.savefig(title)
             plt.show()
+
+    def plot_boundary(self, ax, a, b, c, d):
+        if c != 0:
+            x = np.arange(-3, 3, 1)  # generate a mesh
+            y = np.arange(-3, 3, 1)
+            x, y = np.meshgrid(x, y)
+            z = (-a * x - b * y - d) / c
+        elif b != 0:
+            x = np.arange(-3, 3, 1)  # generate a mesh
+            z = np.arange(-3, 3, 1)
+            x, z = np.meshgrid(x, z)
+            y = (-a * x - c * z - d) / b
+        else:
+            y = np.arange(-3, 3, 1)  # generate a mesh
+            z = np.arange(-3, 3, 1)
+            y, z = np.meshgrid(y, z)
+            x = (-b * y - c * z - d) / a
+
+        ax.plot_surface(x, y, z, color='c', alpha=0.3)  # plot a 3d surface plot
