133a58541dc88f6c4e35ad90a279ea6f7b0e5afb
Azucena López <blankazucenalg@gmali.com>
2016-01-15 02:56:33 -0600

    Perceptron, vowels recognition

--- Pr3-Adaline/config.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-weights = None
-#weights = np.matrix([[1e-300, 1e-300, 1e-300]])
-threshold = None
-#threshold = np.matrix([[0]])
-alpha = None
-#alpha = 0.2
-desired_error = 1e-4
